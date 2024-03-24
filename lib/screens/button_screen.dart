0093170feb5f350dce0d0fcd47b6fb8fb4c63dad
olamidotune <davidegundeyi@gmail.com>
2024-03-24 23:12:39 +0100

    first commit

---
+++ lib/screens/button_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../bloc/counter_bloc.dart';
+
+class ButtonScreen extends StatelessWidget {
+  const ButtonScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final counterBloc = BlocProvider.of<CounterBloc>(context);
+    return Scaffold(
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              counterBloc.add(CounterIncremented());
+            },
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              // counterCubit.decrement();
+            },
+            tooltip: 'Increment',
+            child: const Icon(Icons.minimize),
+          ),
+        ],
+      ),
+    );
+  }
+}
